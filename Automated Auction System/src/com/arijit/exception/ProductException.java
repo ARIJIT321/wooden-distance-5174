3fce48aeac08f934ec8cf625ce79a217daf20528
Arijit Bhatta <103993626+ARIJIT321@users.noreply.github.com>
2023-01-29 19:38:47 +0530

    Update ProductException.java

--- Automated Auction System/src/com/arijit/exception/ProductException.java
+++ Automated Auction System/src/com/arijit/exception/ProductException.java
@@ -4,12 +4,10 @@ public class ProductException extends Exception{
 
 	public ProductException() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 	
 	
 	public ProductException(String mess) {
 		super(mess);
-		// TODO Auto-generated constructor stub
 	}
 }
