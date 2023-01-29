78794957cf1a388b081e658e27083234e5cacfb3
Arijit Bhatta <103993626+ARIJIT321@users.noreply.github.com>
2023-01-29 19:39:06 +0530

    Update SellerException.java

--- Automated Auction System/src/com/arijit/exception/SellerException.java
+++ Automated Auction System/src/com/arijit/exception/SellerException.java
@@ -4,12 +4,10 @@ public class SellerException extends Exception{
 
 	public SellerException() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 	
 	public SellerException(String mess) {	
 		super(mess);
-		// TODO Auto-generated constructor stub
 	}
 	
 	
