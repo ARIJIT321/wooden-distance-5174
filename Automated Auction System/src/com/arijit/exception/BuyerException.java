d18827d68b25fe9ba57a0c0c7006e6343cddbe2b
Arijit Bhatta <103993626+ARIJIT321@users.noreply.github.com>
2023-01-29 19:38:25 +0530

    Update BuyerException.java

--- Automated Auction System/src/com/arijit/exception/BuyerException.java
+++ Automated Auction System/src/com/arijit/exception/BuyerException.java
@@ -4,13 +4,11 @@ public class BuyerException extends Exception{
 
 	public BuyerException() {
 		super();
-		// TODO Auto-generated constructor stub
 
 	}
 	
 	public BuyerException(String mess) {	
 		super(mess);
-		// TODO Auto-generated constructor stub
 	}
 	
 	
