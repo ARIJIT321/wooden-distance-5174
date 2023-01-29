aaf1d4dca5e3461547b2e2640f383625132183f7
Arijit Bhatta <103993626+ARIJIT321@users.noreply.github.com>
2023-01-29 19:37:53 +0530

    Update AdminException.java

--- Automated Auction System/src/com/arijit/exception/AdminException.java
+++ Automated Auction System/src/com/arijit/exception/AdminException.java
@@ -4,12 +4,10 @@ public class AdminException extends Exception{
 
 	public AdminException() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
 	public AdminException(String message) {
 		super(message);
-		// TODO Auto-generated constructor stub
 	}
 
 }
